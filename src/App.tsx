882a840a028553b9ceb5398b003afee4f6731b12
Gabriel Cassiano <gfc@lionx.com.br>
2022-10-16 21:15:05 -0300

    feat: added to style screen, icon logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Logo } from './Logo';
 import './styles/global.css';
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div  className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
+      <Logo />
+    </div>
+  )
 }
